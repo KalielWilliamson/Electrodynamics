58709cfe75e36b450049204359a3980ccac3e094
Kaliel Williamson <kalielwilliamson@gmail.com>
2016-01-12 16:18:14 -0500

    psuedo-continuous 3D space
    I made it so that points are placed into the space with double
    coordinates instead of integer ones.

--- ModelSpace.cpp
+++ ModelSpace.cpp
@@ -58,20 +58,20 @@ class KTree
 		{
 			return this->_root;
 		}
-		bool arePointsSame(point cell1, point cell2)
+		bool arePointsSame(point cell1, double cell2[3])
 		{
 			// Compare individual pointinate values
-			for (int i = 0; i < k; ++i)
-				if (cell1.getLocation()[i] != cell2.getLocation()[i])
+			for (int i = 0; i < k; i++)
+				if (cell1.getLocation()[i] != cell2[i])
 					return false;
 			return true;
 		}
-		Node* searchRecords(Node* root, point cell, unsigned depth)
+		Node* searchRecords(Node* root, double loc[3], unsigned depth)
 		{
 			// Base cases
 			if (root == NULL)
 				return NULL;
-			if (arePointsSame(root->cell,cell))
+			if (arePointsSame(root->cell,loc))
 				return root;
 
 			// Current dimension is computed using current depth and total
@@ -79,12 +79,12 @@ class KTree
 			unsigned cd = depth % k;
 
 			// Compare point with root with respect to cd (Current dimension)
-			if (cell.getLocation()[cd] < root->cell.getLocation()[cd])
-				return searchRecords(root->leftChild, cell, ++depth);
+			if (loc[cd] < root->cell.getLocation()[cd])
+				return searchRecords(root->leftChild, loc, ++depth);
 
-			return searchRecords(root->rightChild, cell, depth + 1);
+			return searchRecords(root->rightChild, loc, depth + 1);
 		}
-		Node* searchRecordsApprox(Node* root, point cell, unsigned depth)
+		Node* searchRecordsApprox(Node* root, double cell[3], unsigned depth)
 				{
 					// Base cases
 					if (root == NULL)
@@ -97,12 +97,12 @@ class KTree
 					unsigned cd = depth % k;
 
 					// Compare point with root with respect to cd (Current dimension)
-					if (cell.getLocation()[cd] < root->cell.getLocation()[cd])
+					if (cell[cd] < root->cell.getLocation()[cd])
 						return searchRecords(root->leftChild, cell, ++depth);
 
 					return searchRecords(root->rightChild, cell, depth + 1);
 				}
-		Node* search(Node* root, point cell)
+		Node* search(Node* root, double cell[3])
 		{
 			// Pass current depth as 0
 			return searchRecords(root, cell, 0);
@@ -138,37 +138,57 @@ class space
 		}
 		void add(point cell)
 		{
-			if(cell.getX() <= length && cell.getY() <= width && cell.getZ() <= height)
+			grid.insert(grid.getRoot(),cell);
+		}
+		point search(double searchLocation[3])
+		{
+			Node *getN = grid.search(grid.getRoot(),searchLocation);
+			if(getN == NULL)
 			{
-				grid.insert(grid.getRoot(),cell);
+				point *p = new point();
+				return *p;
 			}
-		}
-		point search(point searchPoint)
-		{
-			Node *getN = grid.search(grid.getRoot(),searchPoint);
 			return getN->cell;
 		}
-		point search(double x, double y, double z)
+		point search(double x, double y, double z=0)
 		{
-			point p;
-			p.setX(x);
-			p.setY(y);
-			p.setZ(z);
-			Node *getN = grid.search(grid.getRoot(),p);
+			double searchLocation[] = {x,y,z};
+			Node *getN = grid.search(grid.getRoot(),searchLocation);
 			return getN->cell;
+		}
+		void setGrid(double distance)
+		{
+			for(double i=0;i<length;i+=distance)
+				{
+					for(double j=0;j<width;j+=distance)
+					{
+						for(double k=0;k<height;k+=distance)
+						{
+							point p;
+							p.setX(i);
+							p.setY(j);
+							p.setZ(k);
+							physics *r = p.getProperties();
+							r->setTemp(i+j);
+							r->setVector(i,j,k);
+							this->add(p);
+						}
+					}
+				}
 		}
 		void update(point changeCell)
 		{
-			if(changeCell.getX() <= length && changeCell.getY() <= width && changeCell.getZ() <= height)
+			double dCell[] = {changeCell.getX(),changeCell.getY(),changeCell.getZ()};
+			if(dCell[0] <= length && dCell[1] <= width && dCell[2] <= height)
 			{
 				//find the node in the k-tree with the same location
-				Node *getN = grid.search(grid.getRoot(),changeCell);
+				Node *getN = grid.search(grid.getRoot(),dCell);
 				//update everything else
 				getN->cell=changeCell;
 			}
 		}
 
-		space(int lengthBound, int widthBound, int heightBound=0)
+		space(int lengthBound, int widthBound, int heightBound=1)
 		{
 			setLength(lengthBound);
 			setWidth(widthBound);
@@ -186,30 +206,12 @@ int main()
 	double z;
 	double temp;
 	space model(length,width);
-	point cells[length][width];
-	for(int i=0;i<length;i++)
-	{
-		for(int j=0;j<width;j++)
-		{
-			point p;
-			p.setX(i);
-			p.setY(j);
-			p.setZ(0);
-			physics *r = p.getProperties();
-			r->setTemp(i+j);
-			r->setVector(i,j,3);
-			cells[i][j] = p;
-			model.add(p);
-		}
-	}
+	model.setGrid(1);
 	for(int i=0;i<length/5;i++)
 		{
 			for(int j=0;j<width/5;j++)
 			{
-				point p1;
-				p1.setX(i);
-				p1.setY(j);
-				point p2 = model.search(p1);
+				point p2 = model.search(i,j);
 				physics *r = p2.getProperties();
 				temp = r->getTemp();
 				x = r->getX();
