932991d5e9f7cff6c65580e02cddad67b71b5e50
Kaliel Williamson <kalielwilliamson@gmail.com>
2016-01-13 02:12:18 -0500

    beginning of 3D interpolation from n-ary tree mesh
    an n-ary BST is used to hold measurement samples with large coordinate
    distances. A 3D array interpolates physics data from the closest mesh
    data points and performs a volumetric intersection to calculate a likely
    physics parameter, decompressing the physics information.

--- ModelSpace.cpp
+++ ModelSpace.cpp
@@ -8,17 +8,27 @@
 #include <string>
 #include <iostream>
 #include <exception>
+#include <stack>
 #include "Points.cpp"
 #include "Physics.cpp"
 class Node
 {
 	public:
 		point cell;
+		Node *parent;
 		Node *leftChild;
 		Node *rightChild;
 		Node(point newCell)
 		{
 			cell = newCell;
+			parent = NULL;
+			leftChild = NULL;
+			rightChild = NULL;
+		}
+		Node(Node *previous,point newCell)
+		{
+			cell = newCell;
+			parent = previous;
 			leftChild = NULL;
 			rightChild = NULL;
 		}
@@ -34,7 +44,7 @@ class KTree
 		{
 			// Tree is empty?
 			if (root == NULL)
-			   return new Node(cell);
+			   return new Node(root,cell);
 
 			// Calculate current dimension (cd) of comparison
 			unsigned cd = depth % k;
@@ -70,19 +80,41 @@ class KTree
 		{
 			// Base cases
 			if (root == NULL)
-				return NULL;
+				return root->parent;
 			if (arePointsSame(root->cell,loc))
 				return root;
 
 			// Current dimension is computed using current depth and total
 			// dimensions (k)
-			unsigned cd = depth % k;
+			int cd = depth % k;
+			bool a = loc[cd] < root->cell.getLocation()[cd];
+			bool b=false;
+			bool c=false;
+			bool d=false;
+			if(root->leftChild != NULL)
+				b = loc[cd] > root->leftChild->cell.getLocation()[cd];
+			if(root != NULL)
+				c = loc[cd] > root->cell.getLocation()[cd];
+			if(root->rightChild != NULL)
+				d = loc[cd] < root->rightChild->cell.getLocation()[cd];
+			if((a && b) || (c && d))
+				return root;
 
 			// Compare point with root with respect to cd (Current dimension)
-			if (loc[cd] < root->cell.getLocation()[cd])
+			if(root->leftChild == NULL && root->rightChild == NULL)
+			{
+				return root;
+			}
+			if (a)
 				return searchRecords(root->leftChild, loc, ++depth);
+			if (loc[cd] >= root->cell.getLocation()[cd])
+			{
+				return searchRecords(root->rightChild, loc, ++depth);
+			}
+			if(cd == 1)
+				std::cout <<"found 3"<<std::endl;
 
-			return searchRecords(root->rightChild, loc, depth + 1);
+			//return root;
 		}
 		Node* searchRecordsApprox(Node* root, double cell[3], unsigned depth)
 				{
@@ -119,30 +151,32 @@ class KTree
 class space
 {
 	private:
-		int length;
-		int width;
-		int height;
-		KTree grid; //2D k-search tree of points
+		double length;
+		double width;
+		double height;
+		double gridResolution;
+		point grid[100][100][100];
+		KTree mesh; //2D k-search tree of points
 	public:
-		void setWidth(int w)
+		void setWidth(double w)
 		{
 			width = w;
 		}
-		void setLength(int l)
+		void setLength(double l)
 		{
 			length = l;
 		}
-		void setHeight(int h)
+		void setHeight(double h)
 		{
 			height = h;
 		}
 		void add(point cell)
 		{
-			grid.insert(grid.getRoot(),cell);
+			mesh.insert(mesh.getRoot(),cell);
 		}
 		point search(double searchLocation[3])
 		{
-			Node *getN = grid.search(grid.getRoot(),searchLocation);
+			Node *getN = mesh.search(mesh.getRoot(),searchLocation);
 			if(getN == NULL)
 			{
 				point *p = new point();
@@ -150,13 +184,13 @@ class space
 			}
 			return getN->cell;
 		}
-		point search(double x, double y, double z=0)
+		point search(double x, double y, double z)
 		{
 			double searchLocation[] = {x,y,z};
-			Node *getN = grid.search(grid.getRoot(),searchLocation);
+			Node *getN = mesh.search(mesh.getRoot(),searchLocation);
 			return getN->cell;
 		}
-		void setGrid(double distance)
+		void setMesh(double distance)
 		{
 			for(double i=0;i<length;i+=distance)
 				{
@@ -182,36 +216,134 @@ class space
 			if(dCell[0] <= length && dCell[1] <= width && dCell[2] <= height)
 			{
 				//find the node in the k-tree with the same location
-				Node *getN = grid.search(grid.getRoot(),dCell);
+				Node *getN = mesh.search(mesh.getRoot(),dCell);
 				//update everything else
 				getN->cell=changeCell;
 			}
 		}
-
-		space(int lengthBound, int widthBound, int heightBound=1)
+		Node* getChild(Node *N,int lr)
 		{
-			setLength(lengthBound);
-			setWidth(widthBound);
-			setHeight(heightBound);
+			//base case
+			if(N->leftChild == NULL && N->rightChild == NULL)
+				return NULL;
+			if(N->leftChild != NULL && lr == 1)
+			{
+				return N->leftChild;
+			}
+			else
+			{
+				return N->rightChild;
+			}
+			if(N->leftChild != NULL && lr == 2)
+			{
+				return N->rightChild;
+			}
+			else
+			{
+				return N->leftChild;
+			}
+
+		}
+		physics* getClosest(double loc[3])
+		{
+			//method for finding closest 3 nodes on tree and returning physics point
+			//grab node
+			int ind = 0;
+			physics *closest[4];
+			Node *getN = mesh.search(mesh.getRoot(),loc);
+			closest[0] = getN->cell.getProperties();
+			closest[1] = getN->parent->cell.getProperties();
+			closest[2] = getN->leftChild->cell.getProperties();
+			closest[3] = getN->rightChild->cell.getProperties();
+			for(int i=0;i<4;i++)
+				if(closest[i] != NULL)
+					ind++;
+			physics *clo[ind];
+			ind=0;
+			for(int i=0;i<4;i++)
+					if(closest[i] != NULL)
+						clo[ind++]=closest[i];
+
+			return *clo;
+		}
+		void setGridResolution(double res)
+		{
+			gridResolution = res;
+		}
+		void interpolateGrid()
+		{
+			point *p1;
+			double temp;
+			for(int i=0;i<length*gridResolution;i++)
+			{
+				for(int j=0;j<width*gridResolution;j++)
+				{
+					for(int k=0;k<height*gridResolution;k++)
+					{
+						p1 = new point(i/gridResolution,j/gridResolution,k/gridResolution);
+						temp = interpolatePoint(p1);
+						p1->getProperties()->setTemp(temp);
+						grid[i][j][k] = *p1;
+					}
+				}
+			}
+		}
+		double interpolatePoint(point *p)
+		{
+			double x = p->getX();
+			double y = p->getY();
+			double z = p->getZ();
+			double loc[] = {x,y,z};
+			physics *p2 = getClosest(loc);
+			//find volume intersect
+			double ave=0;
+			for(int i=0;i<4;i++)
+			{
+				if(&p2[i] != NULL)
+				{
+					x++;
+					ave+=p2[i].getTemp();
+				}
+			}
+			return ave=ave/x;
+		}
+		point getGridPoint(int x, int y, int z)
+		{
+			return grid[x][y][z];
+		}
+		space(double lengthBound, double widthBound, double heightBound=1,double res=1)
+		{
+			length = lengthBound;
+			width = widthBound;
+			height = heightBound;
+			gridResolution = res;
 		}
 		~space(){}
 };
 
 int main()
 {
-	int length = 100;
-	int width = 100;
+	std::cout<<"hey"<<std::endl;
+	double length = 100;
+	double width = 100;
+	double height = 5;
+	double gridResolution = 1;
+	double meshResolution = 0.5;
 	double x;
 	double y;
 	double z;
+	point p2;
+	int size = 10;
 	double temp;
-	space model(length,width);
-	model.setGrid(1);
-	for(int i=0;i<length/5;i++)
+	space *model = new space(length,width,height,gridResolution);
+	model->setMesh(meshResolution);
+	for(double i=0;i<size;i+=gridResolution)
+	{
+		for(double j=0;j<size;j+=gridResolution)
 		{
-			for(int j=0;j<width/5;j++)
+			for(double k=0;k<size;k+=gridResolution)
 			{
-				point p2 = model.search(i,j);
+				p2 = model->search(i,j,k);
 				physics *r = p2.getProperties();
 				temp = r->getTemp();
 				x = r->getX();
@@ -221,5 +353,29 @@ int main()
 						x<<","<<y<<","<<z<<")"<<std::endl;
 			}
 		}
+	}
+	model->interpolateGrid();
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			//for(int k=0;k<size;k++)
+			//{
+				p2 = model->getGridPoint(i,j,0);
+				temp = p2.getProperties()->getTemp();
+				std::cout<<"("<<p2.getX()<<","<<p2.getY()<<","<<p2.getZ()<<") T:"<<temp<<" ";
+			//}
+		}
+		std::cout<<std::endl;
+	}
+	/*p2 = model.search(5,20,2);
+	physics *r = p2.getProperties();
+	temp = r->getTemp();
+	x = r->getX();
+	y = r->getY();
+	z = r->getZ();
+	std::cout <<"("<< p2.getX() <<","<<p2.getY()<<","<<p2.getZ()<<"): T-"<<temp<<" P("<<
+			x<<","<<y<<","<<z<<")"<<std::endl;*/
+	//physics closest[] = getClosest(P);
 }
 
